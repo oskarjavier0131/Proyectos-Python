e2b1021284118acd778dd547923e3d36ebc3d0c3
Oscar Javier Castelblanco Quintero <lubianka01@gmail.com>
2020-08-20 11:59:26 -0500

    cambio

---
+++ Conversor_de_moneda.py
@@ -0,0 +1,8 @@
+pesos = input("¿Cuanto pesos Colombianos tienes?: ")
+pesos = float(pesos)
+valor_dolar = 3875
+dolares = pesos / valor_dolar
+dolares = round(dolares, 2)
+dolares = str(dolares)
+print("Tienes $" + dolares + " " + "dolares") 
+ 
