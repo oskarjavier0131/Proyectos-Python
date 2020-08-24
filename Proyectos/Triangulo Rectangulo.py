9e6c8dc582f8f427c1fdbaff4e0aea876050c780
Oscar Javier Castelblanco Quintero <lubianka01@gmail.com>
2020-08-23 20:54:14 -0500

    Se incluye proyecto Triangulo Rectangulo

---
+++ Proyectos/Triangulo Rectangulo.py
@@ -0,0 +1,12 @@
+    ## AREA Y PERIMETRO DEL TRIANGULO RECTANGULO
+
+base = input("Ingrese la base del triangulo: ")
+base = int(base)
+altura = input("Ingrese la altura del triangulo: ")
+altura = int(altura)
+area = (base * altura)/2
+perimetro = (base*2) + (altura*2)
+area = str(area)
+perimetro = str(perimetro)
+print("El area del Triangulo Rectangulo  es" + " " + area + " " + "y su perimetro es" + " " + perimetro) 
+
