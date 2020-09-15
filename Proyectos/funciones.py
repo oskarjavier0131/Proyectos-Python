2ac810c17b08626147488be3af4ad865f611848f
Oscar Javier Castelblanco Quintero <lubianka01@gmail.com>
2020-09-15 01:53:31 -0500

    Se agraga prueba de primalidad

--- Proyectos/funciones.py
+++ Proyectos/funciones.py
@@ -40,4 +40,58 @@
 # for contador in range(100) :
 #     print(contador)
 
-  
+
+
+
+# producto_a = 10
+# producto_b = 5
+# producto_c = 7
+
+# menu = """ 
+# SISTEMA DE IMVENTARIO JAVIER 😎😎😎
+
+# ¿QUE PRODUCTO VAS A COMPRAR?
+
+# 1 - Producto A
+# 2 - Producto B
+# 3 - Producto C
+# 4 - RESUMEN
+
+# ELIGE UNA OPCIÒN: """
+
+# opcion = int(input(menu))
+
+# if opcion == 1:
+#     while producto_a > 5:
+#         producto_a = producto_a -1
+#         print(producto_a)
+# elif opcion == 2:
+#     while producto_b > 3:
+#         producto_b -1
+# elif opcion == 3:
+#     while producto_c > 3:
+#         producto_c -1                
+# else:
+#     print("Eligie una opciòn valida")        
+
+def run():
+    # for contador in range(1000):
+    #     if contador % 2 != 0:
+    #         continue
+    #     print(contador)
+
+    for i in range(10000):
+        print(i)
+        if i == 4567:
+            break
+    
+
+
+if __name__ == "main":
+    run()
+
+
+
+
+
+
